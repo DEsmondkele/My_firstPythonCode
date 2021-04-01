55ed8649cd905cee0e3e1c515f6d0d50cf509389
Igbekele Audu Desmond <79143133+DEsmondkele@users.noreply.github.com>
2021-04-01 05:50:45 +0100

    Add files via upload

---
+++ Exercises_py/__init__.py
@@ -0,0 +1,6 @@
+First_Number = input("Enter your Number")
+Second_Number = input("Enter Your Second Number")
+if First_Number > Second_Number:
+    print("the largest number is", First_Number)
+else:
+    print("The largest number is", Second_Number)
